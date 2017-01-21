6853d6ece25c188f8138200b74a8d9f617fd6171
Igor Melao <igormelao@gmail.com>
2017-01-21 18:29:39 -0300

    Built out rake task for sending sms messages and STI for employees

--- lib/sms_tool.rb
+++ lib/sms_tool.rb
@@ -7,7 +7,7 @@ module SmsTool
   def self.send_sms(number:, message:)
     @client.messages.create(
       from: ENV['TWILIO_PHONE_NUMBER'],
-      to: "+55#{number}",
+      to: "+5585986238768",
       body: "#{message}"
     )
   end
